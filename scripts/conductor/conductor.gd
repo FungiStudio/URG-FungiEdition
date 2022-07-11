aff1c6757f87087662a397a9cef3fc508e9c20bb
ShadowFungi <shadowfungi.meddr@aleeas.com>
2022-07-10 23:16:13 -0600

    start game mode implementation
    added some basics things for a chunithm style game mode

--- scripts/conductor/conductor.gd
+++ scripts/conductor/conductor.gd
@@ -20,3 +20,61 @@ signal measure(position)
 
 func _ready():
 	sec_per_beat = 60 / bpm
+
+
+func _physics_process(_delta):
+	if playing:
+		song_position = get_playback_position() + AudioServer.get_time_since_last_mix()
+		song_position -= AudioServer.get_output_latency()
+		song_position_in_beats = int(floor(song_position / sec_per_beat)) + beats_before_start
+		_report_beat()
+
+
+func _report_beat():
+	if last_reported_beat < song_position_in_beats:
+		if measure > measures:
+			measure = 1
+		emit_signal("beat", song_position_in_beats)
+		emit_signal("measure", measure)
+		last_reported_beat = song_position_in_beats
+		measure += 1
+
+
+func play_with_beat_offset(num):
+	beats_before_start = num
+	$Timer.wait_time = sec_per_beat
+	$Timer.start()
+
+
+func closest_beat(nth):
+	closest = int(round((song_position / sec_per_beat) / nth) * nth)
+	time_off_beat = abs(closest * sec_per_beat - song_position)
+	return Vector2(closest, time_off_beat)
+
+
+func play_from_beat(beat, offset):
+	play()
+	seek(beat * sec_per_beat)
+	beats_before_start = offset
+	
+	var i = 1
+	while i < beats_before_start:
+		measure += 1
+		if measure > measures:
+			measure = 1
+		i += 1
+
+
+
+
+func _on_Timer_timeout():
+	song_position_in_beats += 1
+	if song_position_in_beats < beats_before_start - 1:
+		$Timer.start()
+	elif song_position_in_beats == beats_before_start - 1:
+		$Timer.wait_time = $Timer.wait_time - (AudioServer.get_time_to_next_mix() + AudioServer.get_output_latency())
+		$Timer.start()
+	else:
+		play()
+		$Timer.stop()
+	_report_beat()
